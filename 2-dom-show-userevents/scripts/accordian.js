da26362eec977e7d8931a18f623f7d4827a00edc
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2022-10-14 13:14:53 +0200

    Fungerande accordion

--- 2-dom-show-userevents/scripts/accordian.js
+++ 2-dom-show-userevents/scripts/accordian.js
@@ -1,1 +1,18 @@
 // you should use a loop ...
+
+let accordionContainer = document.getElementsByClassName("accordian-container");
+
+for (let i = 0; i < accordionContainer.length; i++) {
+  accordionContainer[i].addEventListener("click", function() {
+    openAndClose(accordionContainer[i]);
+  });
+}
+
+function openAndClose(accordion) {
+  for (let i = 0; i < accordionContainer.length; i++) {
+    if (accordionContainer[i] != accordion) {
+      accordionContainer[i].classList.remove("active");
+    }
+  }
+   accordion.classList.toggle("active");
+}
